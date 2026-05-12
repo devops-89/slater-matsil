2a2d9b841ccf8a29427b157f132b9f181b127e17
yashwinDigixito <yashwin.sharma@digixito.org>
2026-05-12 17:07:57 +0530

    ADD About

--- components/layouts/insights-details-layout/InsightsDetailsContentSection.tsx
+++ components/layouts/insights-details-layout/InsightsDetailsContentSection.tsx
@@ -105,35 +105,83 @@ const InsightsDetailsContentSection = ({
       <Container maxWidth="lg">
         {/* About and Watermark row */}
         <Stack
-          direction="row"
-          alignItems="center"
-          justifyContent="space-between"
-          sx={{ mb: 8, position: "relative" }}
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+        sx={{
+          mb: 8,
+          position: "relative",
+          overflow: "hidden",
+          minHeight: { md: 220 },
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: { xs: "100%", md: "45%" },
+            zIndex: 2,
+            position: "relative",
+          }}
         >
-
+          <Box sx={{ mb: 2 }}>
+            <HeadingStar title="About" />
+          </Box>
           <Typography
             sx={{
-              display: { xs: "none", md: "flex" },
-              mx:20,
               fontFamily: tradeGothic.style.fontFamily,
               fontWeight: 700,
-              fontSize: { md: 140 },
-              textTransform: "lowercase",
-              pointerEvents: "none",
-              userSelect: "none",
-              opacity: 0.1,
-              lineHeight: 1,
+              fontSize: { xs: 24, md: 36 },
+              lineHeight: 1.05,
+              color: COLORS.PRIMARY_BLUE,
+              textTransform: "capitalize",
             }}
           >
-            <Box component="span" sx={{ color: COLORS.PRIMARY_BLUE }}>
-              slater
+            Provided By{" "}
+            <Box
+              component="span"
+              sx={{
+                color: COLORS.PRIMARY_GREEN,
+              }}
+            >
+              Slater
             </Box>
-            <Box component="span" sx={{ color: COLORS.PRIMARY_GREEN }}>
-              matsil
+            <br />
+            <Box
+              component="span"
+              sx={{
+                color: COLORS.PRIMARY_GREEN,
+              }}
+            >
+              Matsil, LLP
             </Box>
           </Typography>
-        </Stack>
-
+        </Box>
+          <Typography
+          sx={{
+            display: { xs: "none", md: "block" },
+            position: "absolute",
+            right: "5%",
+            top: "20%",
+            transform: "translateY(-50%)",
+            fontFamily: tradeGothic.style.fontFamily,
+            fontWeight: 700,
+            fontSize: "140px",
+            textTransform: "lowercase",
+            pointerEvents: "none",
+            userSelect: "none",
+            opacity: 0.08,
+            lineHeight: 0.9,
+            whiteSpace: "nowrap",
+            zIndex: 1,
+          }}
+        >
+          <Box component="span" sx={{ color: COLORS.PRIMARY_BLUE }}>
+            slater
+          </Box>
+          <Box component="span" sx={{ color: COLORS.PRIMARY_GREEN }}>
+            matsil
+          </Box>
+        </Typography>
+      </Stack>
         {/* Region (USA) section */}
         <Box sx={{ mb: 10 }}>
           <Typography
